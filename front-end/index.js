45eabc679ba61a48bade58d5056369d7d5dd2ef1
Max Somerville <85084032+maxthemus@users.noreply.github.com>
2023-02-03 15:31:01 +1300

    Adding comments to code

--- front-end/index.js
+++ front-end/index.js
@@ -1,19 +1,24 @@
 //Variables
-let inGame = false;
-let text = "";
-let textArray = [];
+let inGame = false; //Variable to keep if user is in game or not
+let text = ""; //Holds text user is typing in game
+let textArray = []; //Holds the individual characters for the text in Array
 
 //Functions
+/**
+Funtion getText() will send an API request to the backend to recieve a random snippet of code for the game
+*/
 function getText() {
-    fetch('http://localhost:4000/api/v1/text').then((payload) => {
-        return payload.json();
-    }).then((data) => {
+    //Fetch is used to send API requests
+    fetch('http://localhost:4000/api/v1/text').then((payload) => { 
+        return payload.json(); //Takes what is sent back and packages it into JSON
+    }).then((data) => { //the parameter is data is the JSON 
         inGame = true; //Setting that we are in the gameA
         text = data.text;
         textArray = text.split("");
 
         addTextToScreen();
     }).catch((err) => {
+        //ERROR handling
         console.log(err);
         alert("Something Went Wrong on FETCH REQUEST");
     });
