c21f62c390f02aeef978290615b92da57f86cd15
maxthemus <max.somerville62@gmail.com>
2023-04-17 12:45:21 +1200

    Merged front-end and backend

--- back-end/index.js
+++ back-end/index.js
@@ -12,7 +12,7 @@ const texts = [
 
 //IMPORTS 
 const express = require('express');
-const cors = require("cors");
+const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
