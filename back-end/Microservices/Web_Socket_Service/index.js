4a254b6f1420d24d492eab540d9238be12190b19
Max Somerville <max.somerville62@gmail.com>
2023-05-14 08:37:11 +1200

    Web Socket Service - New socket authentication implemented
    The Web Socket Service can accept new socket connections and authenticates them. If the socket auth failed then the socket is closed.

--- back-end/Microservices/Web_Socket_Service/index.js
+++ back-end/Microservices/Web_Socket_Service/index.js
@@ -28,12 +28,122 @@ const socketServer = new WebSocket.Server({ server });
 //Starting server
 server.listen(PORT, () => {
     console.log("Web Socket Service has started on PORT : " + PORT);
-})
-
-socketServer.on("open", () => {
-    console.log("Client has connected");
 });
+
+//Authentication key
+const fs = require('fs');
+const key = fs.readFileSync("../../Authentication/secret.key", "utf-8").trim();
+
+//JSON web tokens
+const jwt = require("jsonwebtoken");
+
+
+//Handling Web Sockets
+//Maps user Sockets -> UserId
+const CLIENT_SOCKETS = new Map();
+
+socketServer.on("connection", (socket) => {
+    console.log("Client has connected");
+
+    /**
+     * All socket connections have to be handled inside this block
+     * or you will not be able to identify what socket the message has
+     * come from
+     */
+    socket.on("message", (buffer) => {
+        console.log(buffer);
+        const message = JSON.parse(buffer); //HANDLE IF BODY IS NOT JSON WILL THROW ERROR AND RESULT IN CRASH
+        console.log(message);
+
+        //If the socket isn't in the map then it isn't authenticated
+        if(!CLIENT_SOCKETS.has(socket)) {
+            //Checking for message type
+            //Drops message if no type
+            if("type" in message)  {
+                //If invalid type then just drop the packet
+                switch(message.type) {
+                    case "AUTH":
+                        handleAuthenticationMessage(socket, message);
+                        break;
+                }
+            } 
+        } else {
+            socket.send(JSON.stringify({
+                type: "message",
+                message: "Already authenticated"
+            }));
+        }
+    });
+
+});
+
 
 socketServer.on("close", () => {
+
     console.log("Client has closed connection");
 });
+
+
+
+
+
+//FUNCTIONS
+function handleAuthenticationMessage(socket, message) {
+    if("token" in message) {
+        let authenticated = authenticateSocket(socket, message.token);
+
+        if(!authenticated) {
+            //Closing socket due to auth
+            socket.close(3001, JSON.stringify({
+                type: "AUTH",
+                message: "Authentication failed"
+            })); 
+        } else {
+            socket.send(JSON.stringify({
+                type: "AUTH",
+                authenticated: true
+            }));
+        }
+    } 
+}
+
+/**
+ * Uses JWT to authenticate socket 
+ * @param {socket} socket 
+ * @param {jwt} token 
+ */
+function authenticateSocket(socket, token) {
+    console.log("Authenticating token");
+    let authenticated = false;
+
+    try {
+        const decoded = jwt.verify(token, key);
+        console.log("Valid AUTH");
+        console.log(decoded);
+        //and we want to add socket to map
+        if("userId" in decoded) {
+            addSocketToMap(socket, decoded.userId);
+            authenticated = true;
+        }
+    } catch(error) {
+        console.log("Invalid token");
+    }
+    return authenticated;
+} 
+
+/**
+ * maps socket to userId 
+ * @param {socket} socket 
+ * @param {uuid as String} userId 
+ */
+function addSocketToMap(socket, userId) {
+    CLIENT_SOCKETS.set(socket, userId);
+}
+
+/**
+ * removes are socket to userId map 
+ * @param {socket} socket 
+ */
+function removeSocketFromMap(socket) {
+    CLIENT_SOCKETS.delete(socket);
+}
--- front-end/socket.js
+++ front-end/socket.js
@@ -1,3 +1,5 @@
+const token = "yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI0YjllMmRjNC0xOWQ0LTRiMTgtODVlZS0xNGEyYTM1NmJlMGIiLCJpYXQiOjE2ODQwMDIzMzN9.smUSMtzzWPggwK6LfI-AvC2UcSZYnOqxTTm-hh0V5Xw";
+const userID = "6fe5bacc-f1bf-11ed-a05b-0242ac120003";
 //Opening socket connection
 const socket = new WebSocket('ws://127.0.0.1:3053');
 
@@ -7,5 +9,34 @@ socket.addEventListener('open', (event) => {
 });
 
 socket.addEventListener("close", (event) => {
+    console.log(event);
+
+    switch(event.code) {
+        case 3001:
+            console.log("Authentication failed");
+            break;
+    }
     console.log("Socket has been closed");
-})
+});
+
+socket.addEventListener("message", (event) => {
+    console.log(event.data);
+    const message = JSON.parse(event.data);
+    console.log(message);
+});
+
+
+//FUNCTIONS 
+async function authenticateSocket() {
+    if(socket.readyState == WebSocket.OPEN) {
+        const messageStr = JSON.stringify({
+            type: "AUTH",
+            token: token
+        });
+
+        socket.send(messageStr);
+        console.log("Auth Sent"); 
+    } else {
+        console.log("Socket is not open");
+    }
+}
